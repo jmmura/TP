f32902289f80bd5e660e2835c5b4487b285f5112
jmmura <115638944+jmmura@users.noreply.github.com>
2023-01-10 10:40:51 +0100

    Update Stage.java

--- src/Stage.java
+++ src/Stage.java
@@ -4,9 +4,10 @@ import java.util.*;
 
 public class Stage extends Activite{
 
-
-    public Stage(String Nom, String type,Horaire h) {
-        super(Nom, type, h);
-
+    private int prix = 20;
+    public static Map<String,Activite> stages = new HashMap<>();
+    public Stage(String Nom, String type,Horaire h,double prix) {
+        super(Nom, type, h,prix);
+        stages.put(Nom,this);
     }
 }
