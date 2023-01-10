3f0473a3757000fe9948757580e955a6246d4ff4
jmmura <115638944+jmmura@users.noreply.github.com>
2023-01-10 10:41:57 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,25 +6,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Stage stage1 = new Stage("stage1","stage",new Horaire(LocalTime.now(),LocalTime.now(),LocalDate.now()));
-        Stage stage2 = new Stage("stage2","stage",new Horaire(LocalTime.now(),LocalTime.now(),LocalDate.now()));
-        Stage stage3 = new Stage("stage3","stage",new Horaire(LocalTime.now(),LocalTime.now(),LocalDate.now()));
-        Stage stage4 = new Stage("stage4","stage",new Horaire(LocalTime.now(),LocalTime.now(),LocalDate.now()));
-        new Activite("stage2","stage", new Horaire(LocalTime.of(2,2,2),LocalTime.of(3,3,3),LocalDate.of(2023,5,12)));
-        boolean prog = true;
-        System.out.printf("Nouvelle personne %n Entrer nom%n");
-        Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-        Personne p = new Personne(s);
-        Activite.afficherProgramme();
-        System.out.println("A quelles activité voulez-vous vous inscrire?");
-        Activite.afficheActivites();
-        String[] choix = sc.nextLine().split(",",0);
-        for(String str : choix){
-            Activite.ajoutMembre(str,p);
-        }
-        System.out.println("Membres par activité: ");
-        Activite.afficherMembres();
+        Stage stage1 = new Stage("stage1","stage",new Horaire(LocalTime.of(10,0),LocalTime.of(11,0),Semaine.lundi),50);
+        Stage stage2 = new Stage("stage2","stage",new Horaire(LocalTime.of(13,0),LocalTime.of(14,0), Semaine.mardi),30);
+        Repas repas1 = new Repas("repas1","repas",new Horaire(LocalTime.of(17,0),LocalTime.of(19,0),Semaine.mardi),40);
+        Stage stage4 = new Stage("stage4","stage",new Horaire(LocalTime.of(10,10),LocalTime.of(11,0),Semaine.mercredi),30);
+        new Activite("stage2","repas", new Horaire(LocalTime.of(9,0),LocalTime.of(10,0),Semaine.vendredi),20);
+
+        starter.init();
 
 
     }
